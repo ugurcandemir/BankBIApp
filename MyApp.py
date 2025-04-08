6bc2b572d4933c76100f0e8d36caf1136363e96a
ugurcandemir <ugur.can.demir@hotmail.com>
2025-04-08 19:58:57 +0300

    first commit

---
+++ MyApp.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from PIL import Image
+import plotly.express as px
+from fpdf import FPDF
+from io import BytesIO
+import base64
+import unicodedata
+import os
+from markdown2 import markdown
+from bs4 import BeautifulSoup
+import tempfile
+import pydeck as pdk
+import joblib
